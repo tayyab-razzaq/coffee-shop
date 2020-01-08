3b1154d98f0cefe93f189cb90852c8ceb3472062
Kagaroat <tayyab.razzaq401@gmail.com>
2020-01-08 21:12:55 +0500

    docs: removed completed part of todo docstring for get drinks detail api

--- backend/src/api.py
+++ backend/src/api.py
@@ -58,11 +58,7 @@ def get_drinks():
 """
 @TODO implement endpoint
     GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks}
-    where drinks is the list of drinks
-    or appropriate status code indicating reason for failure
+    it should require the 'get:drinks-detail' permission
 """
 
 
