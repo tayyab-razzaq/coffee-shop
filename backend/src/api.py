775bf79fc06ff1c4689147c7503dcf17efd40623
Kagaroat <tayyab.razzaq401@gmail.com>
2020-01-08 21:35:33 +0500

    docs: removed completed things from docstring for update drink api

--- backend/src/api.py
+++ backend/src/api.py
@@ -107,16 +107,8 @@ def add_drink():
 
 """
 @TODO implement endpoint
-    PATCH /drinks/<id>
-        
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink}
-    where drink an array containing
-    only the updated drink or appropriate status code
-    indicating reason for failure
+    PATCH /drinks/<id>    
+    it should require the 'patch:drinks' permission
 """
 
 
