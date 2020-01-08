c6922512df5d0c3819d20e7e4bc733c3e7c13272
Kagaroat <tayyab.razzaq401@gmail.com>
2020-01-08 21:04:59 +0500

    docs: removed todo docstring for get drinks api

--- backend/src/api.py
+++ backend/src/api.py
@@ -37,15 +37,6 @@ def after_request(response):
 # db_drop_and_create_all()
 
 # ROUTES
-"""
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks}
-    where drinks is the list of drinks
-    or appropriate status code indicating reason for failure
-"""
 
 
 @app.route('/drinks')
